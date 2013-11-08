6c4af7d6d888423914980b2cd6dfed02186ac15b
amyxwho <ahu2@wellesley.edu>
2013-11-08 15:45:33 -0500

    Pandy Diary App

---
+++ PandyDiary/PandyApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class PandyApp : Form
+    {
+        public PandyApp()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
